c3eb36ac4e0e04f923bcea4e71d86f01166e235a
marcae777 <marcae7@gmail.com>
2018-05-25 13:58:13 -0500

    Se realizan los cambios necesarios en las rutas para que el modulo quede instalable en cualquier lugar del servidor

--- setup.py
+++ setup.py
@@ -2,16 +2,23 @@
 import os
 from numpy.distutils.core import setup, Extension
 
+ext1 = Extension(name = 'al',
+                 sources = ['alarmas/alarmas.py'],
+        f2py_options = ['--opt = O3'])
+ext2 = Extension(name = 'fs',
+                 sources = ['alarmas/funciones_sora.py'])
+
 setup(
     name='hidro_alarmas',
     version='0.0.1',
     author='Hidro SIATA',
     author_email='hidrosiata@gmail.com',    
     packages=['alarmas'],
-    package_data={'alarmas':['ensayo.py']},
+    package_data={'alarmas':['al.so','fs.so']},
     url='https://github.com/SIATAhidro/Alarmas.git',
     license='LICENSE.txt',
-    description='Montaje de alarmas',
+    description='Despliegue de archivos para pagina de Alarmas Comunitarias',
     long_description=open('README.md').read(),
     install_requires=[ ],
+    ext_modules=[ext1, ext2],
     )
